6974ceb413b25437e609a597a33eff5614da2079
cubewhy <qby140326@gmail.com>
2026-02-27 13:44:09 +0800

    feat: crates.nvim for rust language support

--- lua/custom/plugins/rust.lua
+++ lua/custom/plugins/rust.lua
@@ -1,33 +1,58 @@
 return {
-  'mrcjkb/rustaceanvim',
-  version = '^8', -- Recommended
-  lazy = false, -- This plugin is already lazy
-  config = function()
-    vim.g.rustaceanvim = {
-      server = {
-        default_settings = {
-          -- rust-analyzer language server configuration
-          ['rust-analyzer'] = {
-            cargo = {
-              allFeatures = true,
-              loadOutDirsFromCheck = true,
-              buildScripts = {
-                enable = true,
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^8', -- Recommended
+    lazy = false, -- This plugin is already lazy
+    config = function()
+      vim.g.rustaceanvim = {
+        server = {
+          default_settings = {
+            -- rust-analyzer language server configuration
+            ['rust-analyzer'] = {
+              cargo = {
+                allFeatures = true,
+                loadOutDirsFromCheck = true,
+                buildScripts = {
+                  enable = true,
+                },
               },
-            },
-            -- Add clippy lints for Rust.
-            checkOnSave = true,
-            procMacro = {
-              enable = true,
-              ignored = {
-                ['async-trait'] = { 'async_trait' },
-                ['napi-derive'] = { 'napi' },
-                ['async-recursion'] = { 'async_recursion' },
+              -- Add clippy lints for Rust.
+              checkOnSave = true,
+              procMacro = {
+                enable = true,
+                ignored = {
+                  ['async-trait'] = { 'async_trait' },
+                  ['napi-derive'] = { 'napi' },
+                  ['async-recursion'] = { 'async_recursion' },
+                },
               },
             },
           },
         },
-      },
-    }
-  end,
+      }
+    end,
+  },
+  {
+    'saecki/crates.nvim',
+    event = { 'BufRead Cargo.toml' },
+    config = function()
+      require('crates').setup {
+        completion = {
+          crates = {
+            enabled = true,
+            max_results = 8, -- The maximum number of search results to display
+            min_chars = 3, -- The minimum number of charaters to type before completions begin appearing
+          },
+        },
+        lsp = {
+          enabled = true,
+          name = 'crates.nvim',
+          on_attach = function(client, bufnr) end,
+          actions = true,
+          completion = true,
+          hover = true,
+        },
+      }
+    end,
+  },
 }
