ff7852c2a908c132bd4cecf32f0afc05efd1f26f
cubewhy <qby140326@gmail.com>
2026-02-27 11:09:52 +0800

    feat: lazygit color

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -1,11 +1,24 @@
 return {
   {
     'kdheepak/lazygit.nvim',
+    lazy = true,
     cmd = { 'LazyGit', 'LazyGitConfig', 'LazyGitCurrentFile', 'LazyGitFilter', 'LazyGitFilterCurrentFile' },
     dependencies = { 'nvim-lua/plenary.nvim' },
     keys = {
       { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
+    config = function()
+      vim.opt.termguicolors = true
+      vim.api.nvim_set_hl(0, 'LazyGitBorder', { link = 'FloatBorder' })
+
+      vim.g.lazygit_floating_window_winblend = 0 -- transparency of floating window
+      vim.g.lazygit_floating_window_scaling_factor = 0.9 -- scaling factor for floating window
+      vim.g.lazygit_floating_window_border_chars = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' } -- customize lazygit popup window border characters
+      vim.g.lazygit_floating_window_use_plenary = 0 -- use plenary.nvim to manage floating window if available
+      vim.g.lazygit_use_neovim_remote = 1 -- fallback to 0 if neovim-remote is not installed
+
+      vim.g.lazygit_on_exit_callback = nil -- optional function callback when exiting lazygit (useful for example to refresh some UI elements after lazy git has made some changes)
+    end,
   },
   {
     'lewis6991/gitsigns.nvim',
