1b4bfd3bd641d79aeb3e8ec0eee932fa733e8d5c
cubewhy <qby140326@gmail.com>
2026-02-27 19:48:40 +0800

    feat: render diagnostics counter on bufferline

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -15,6 +15,11 @@ return {
   },
   opts = {
     options = {
+      diagnostics = 'nvim_lsp',
+      diagnostics_indicator = function(count, level, diagnostics_dict, context)
+        local icon = level:match 'error' and ' ' or ' '
+        return ' ' .. icon .. count
+      end,
       mode = 'buffers',
       offsets = {
         {
