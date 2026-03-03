ba1c89ad812e5515683e30c03e5c5c36205d9a2d
cubewhy <qby140326@gmail.com>
2026-03-03 22:19:39 +0800

    chore: disable osc52 clipboard

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -88,16 +88,16 @@ vim.diagnostic.config {
   jump = { float = true },
 }
 
-vim.g.clipboard = {
-  name = 'OSC 52',
-  copy = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy '+',
-    ['*'] = require('vim.ui.clipboard.osc52').copy '*',
-  },
-  paste = {
-    ['+'] = require('vim.ui.clipboard.osc52').paste '+',
-    ['*'] = require('vim.ui.clipboard.osc52').paste '*',
-  },
-}
+-- vim.g.clipboard = {
+--   name = 'OSC 52',
+--   copy = {
+--     ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+--     ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+--   },
+--   paste = {
+--     ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+--     ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+--   },
+-- }
 
 vim.opt.laststatus = 3
