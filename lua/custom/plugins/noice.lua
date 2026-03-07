902f92ccab21b3c492b362e12d58dd842ee3548d
cubewhy <qby140326@gmail.com>
2026-03-07 11:11:49 +0800

    fix: set vim.lsp.* handler to noice

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -74,6 +74,13 @@ return {
           opts = { skip = true },
         },
       },
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
     },
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
