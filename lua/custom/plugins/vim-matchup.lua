4f095e4f11ac4922a8da43b691001829f70c7359
cubewhy <qby140326@gmail.com>
2026-03-13 17:12:30 +0800

    chore: remove the duplicated match-up config

--- lua/custom/plugins/vim-matchup.lua
+++ lua/custom/plugins/vim-matchup.lua
@@ -2,11 +2,5 @@ return {
   'andymass/vim-matchup',
   init = function()
     vim.g.matchup_treesitter_stopline = 500
-
-    require('match-up').setup({
-      treesitter = {
-        stopline = 500
-      }
-    })
   end,
 }
