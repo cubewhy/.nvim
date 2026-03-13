6d80860cd087b3e17096419fcd14d22d4938413e
cubewhy <qby140326@gmail.com>
2026-03-13 21:03:54 +0800

    chore: add highlight delay for matchup plugin

--- lua/custom/plugins/vim-matchup.lua
+++ lua/custom/plugins/vim-matchup.lua
@@ -3,6 +3,7 @@ return {
   init = function()
     vim.g.matchup_matchparen_enabled = 1
     vim.g.matchup_matchparen_deferred = 1
+    vim.g.matchup_matchparen_hi_delay = 100
     vim.g.matchup_matchparen_offscreen = { method = "popup" }
     vim.g.matchup_treesitter = 0
   end,
