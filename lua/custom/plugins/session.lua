d09cd13ea8e25e2f212670809e7209e0e527a3b9
cubewhy <qby140326@gmail.com>
2026-03-13 17:25:37 +0800

    fix: exclude dap and terminals from session persistance

--- lua/custom/plugins/session.lua
+++ lua/custom/plugins/session.lua
@@ -20,7 +20,12 @@ return {
         callback = function()
           for _, win in ipairs(vim.api.nvim_list_wins()) do
             local buf = vim.api.nvim_win_get_buf(win)
-            if vim.bo[buf].filetype == 'neo-tree' then vim.api.nvim_win_close(win, true) end
+            local ft = vim.bo[buf].filetype
+            local bt = vim.bo[buf].buftype
+
+            if ft == 'neo-tree' or bt == 'terminal' or ft:find('dapui_') then
+              vim.api.nvim_win_close(win, true)
+            end
           end
         end,
       })
