520789698ea3dd7c508a6c6581c8c4562054be95
cubewhy <qby140326@gmail.com>
2026-03-13 17:59:01 +0800

    fix: use the close action in plugin to cleanup on PersistenceSavePre

--- lua/custom/plugins/session.lua
+++ lua/custom/plugins/session.lua
@@ -18,12 +18,15 @@ return {
       vim.api.nvim_create_autocmd('User', {
         pattern = 'PersistenceSavePre',
         callback = function()
+          local ok_dapui, dapui = pcall(require, "dapui")
+          if ok_dapui then dapui.close() end
+
+          local ok_neotree, neotree = pcall(require, "neo-tree.command")
+          if ok_neotree then neotree.execute({ action = "close" }) end
+
           for _, win in ipairs(vim.api.nvim_list_wins()) do
             local buf = vim.api.nvim_win_get_buf(win)
-            local ft = vim.bo[buf].filetype
-            local bt = vim.bo[buf].buftype
-
-            if ft == 'neo-tree' or bt == 'terminal' or ft:find('dapui_') then
+            if vim.bo[buf].buftype == 'terminal' then
               vim.api.nvim_win_close(win, true)
             end
           end
