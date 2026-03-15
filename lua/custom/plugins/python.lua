9f52397f865a15e61ed9b1c9dcbf92d46c256b83
cubewhy <qby140326@gmail.com>
2026-03-15 13:25:18 +0800

    feat: venv-selector

---
+++ lua/custom/plugins/python.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'linux-cultist/venv-selector.nvim',
+    dependencies = {
+      { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } }, -- optional: you can also use fzf-lua, snacks, mini-pick instead.
+    },
+    ft = 'python', -- Load when opening Python files
+    keys = { { '<leader>cv', '<cmd>VenvSelect<cr>', desc = 'Select virtual environment' } }, -- Open picker on keymap
+    opts = {
+      options = {}, -- plugin-wide options
+      search = {}, -- custom search definitions
+    },
+  },
+}
