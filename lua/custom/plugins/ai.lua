e2b5ef1c8f44921d47435927bd9bec1b55612f33
cubewhy <qby140326@gmail.com>
2026-03-15 12:36:12 +0800

    fix: correct codecompanion config

--- lua/custom/plugins/ai.lua
+++ lua/custom/plugins/ai.lua
@@ -10,18 +10,19 @@ return {
       opts = {
         log_level = 'DEBUG', -- or 'TRACE'
       },
-      strategies = {
-        chat = { adapter = 'ollama' },
-        inline = { adapter = 'ollama' },
-      },
-      adapters = {
-        ollama = function()
-          return require('codecompanion.adapters').extend('ollama', {
-            schema = {
-              model = { default = 'qwen2.5-coder:7b' },
-            },
-          })
-        end,
+      interactions = {
+        chat = {
+          adapter = {
+            name = 'ollama',
+            model = 'qwen2.5-coder:7b',
+          },
+        },
+        inline = {
+          adapter = {
+            name = 'ollama',
+            model = 'qwen2.5-coder:7b',
+          },
+        },
       },
     },
   },
