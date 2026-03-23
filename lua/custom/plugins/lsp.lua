7cc2205bef782056863354fc25ca766b829399aa
cubewhy <qby140326@gmail.com>
2026-03-23 17:02:49 +0800

    feat: add symbol-usage plugin

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -334,11 +334,32 @@ return {
       }
     end,
   },
-  -- {
-  --   'zeioth/garbage-day.nvim',
-  --   event = 'VeryLazy',
-  --   opts = {
-  --     aggressive_mode = true,
-  --   },
-  -- },
+  {
+    'Wansmer/symbol-usage.nvim',
+    event = 'BufReadPre',
+    config = function()
+      local function text_format(symbol)
+        local fragments = {}
+
+        -- Indicator that shows if there are any other symbols in the same line
+        local stacked_functions = symbol.stacked_count > 0 and (' | +%s'):format(symbol.stacked_count) or ''
+
+        if symbol.references then
+          local usage = symbol.references <= 1 and 'usage' or 'usages'
+          local num = symbol.references == 0 and 'no' or symbol.references
+          table.insert(fragments, ('%s %s'):format(num, usage))
+        end
+
+        if symbol.definition then table.insert(fragments, symbol.definition .. ' defs') end
+
+        if symbol.implementation then table.insert(fragments, symbol.implementation .. ' impls') end
+
+        return table.concat(fragments, ', ') .. stacked_functions
+      end
+
+      require('symbol-usage').setup {
+        text_format = text_format,
+      }
+    end,
+  },
 }
