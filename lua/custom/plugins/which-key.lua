f6c643b083cf599c5dad9a5b5376bb78061f6ecf
cubewhy <qby140326@gmail.com>
2026-03-23 21:33:28 +0800

    feat: change terminal keys to <num>C+/

--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -24,7 +24,7 @@ return {
       { '<leader>q', group = 'Quit / Session', icon = { icon = '󰈆 ', color = 'red' } },
       { '<leader>s', group = 'Search', icon = { icon = ' ', color = 'cyan' }, mode = { 'n', 'v' } },
       { '<leader>f', group = 'Find', icon = { icon = ' ', color = 'cyan' }, mode = { 'n', 'v' } },
-      { '<leader>t', group = 'Terminal / Test', icon = { icon = ' ', color = 'yellow' } },
+      { '<leader>t', group = 'Test', icon = { icon = '󰙨 ', color = 'yellow' } },
       { '<leader>x', group = 'Diagnostics', icon = { icon = '󱖫 ', color = 'red' } },
       { '<leader>n', group = 'Notifications', icon = { icon = '󰵅 ', color = 'blue' } },
 
