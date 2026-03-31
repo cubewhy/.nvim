943d39d380e34e43c848b08976b8fa81c97de52d
cubewhy <qby140326@gmail.com>
2026-03-31 17:58:02 +0800

    feat: use treesitter on vim-matchup

--- lua/custom/plugins/vim-matchup.lua
+++ lua/custom/plugins/vim-matchup.lua
@@ -4,7 +4,7 @@ return {
     vim.g.matchup_matchparen_enabled = 1
     vim.g.matchup_matchparen_deferred = 1
     vim.g.matchup_matchparen_hi_delay = 100
-    vim.g.matchup_matchparen_offscreen = { method = "popup" }
-    vim.g.matchup_treesitter = 0
+    vim.g.matchup_matchparen_offscreen = { method = 'popup' }
+    vim.g.matchup_treesitter = 1
   end,
 }
