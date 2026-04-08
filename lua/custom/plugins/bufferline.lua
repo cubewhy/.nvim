848ac29ea1c2a97078b9bcd33e37f3613992fd8a
cubewhy <qby140326@gmail.com>
2026-04-08 17:59:53 +0800

    feat: replace empty buffers when open files

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -43,5 +43,28 @@ return {
         desc = 'Delete Buffer',
       },
     },
+    init = function()
+      vim.api.nvim_create_autocmd('BufHidden', {
+        desc = 'Wipe empty, unmodified buffers when they are hidden',
+        callback = function(event)
+          local buf = event.buf
+
+          if not vim.api.nvim_buf_is_valid(buf) then return end
+
+          local name = vim.api.nvim_buf_get_name(buf)
+          local is_modified = vim.bo[buf].modified
+          local buftype = vim.bo[buf].buftype
+          local filetype = vim.bo[buf].filetype
+          local line_count = vim.api.nvim_buf_line_count(buf)
+          local first_line = vim.api.nvim_buf_get_lines(buf, 0, 1, false)[1]
+
+          if name == '' and not is_modified and buftype == '' and filetype == '' and line_count == 1 and first_line == '' then
+            vim.schedule(function()
+              if vim.api.nvim_buf_is_valid(buf) then vim.api.nvim_buf_delete(buf, { force = true }) end
+            end)
+          end
+        end,
+      })
+    end,
   },
 }
