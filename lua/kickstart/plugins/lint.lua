4763562b40d426cf0dffc9b8e986511af7c97680
cubewhy <qby140326@gmail.com>
2026-04-09 18:15:23 +0800

    feat: lint golang with golangci-lint

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -9,7 +9,7 @@ return {
       null_ls.setup {
         sources = {
           null_ls.builtins.diagnostics.markdownlint_cli2,
-
+          null_ls.builtins.diagnostics.golangci_lint,
         },
       }
     end,
