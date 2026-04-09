6fb8e5ca8b0b9860b60169b5d7f7d11a7f981bf0
cubewhy <qby140326@gmail.com>
2026-04-09 21:17:08 +0800

    fix: only render color preview in css

--- lua/custom/plugins/css.lua
+++ lua/custom/plugins/css.lua
@@ -2,13 +2,50 @@ return {
   {
     'catgoose/nvim-colorizer.lua',
     event = 'BufReadPre',
-    opts = {
-      options = {
-        parsers = {
-          css = true,
-          tailwind = { enable = false, lsp = true },
-        }
-      },
-    },
+    config = function()
+      require('colorizer').setup {
+        filetypes = { 'css', 'sass', 'scss' },
+        buftypes = {},
+        user_commands = true,
+        lazy_load = true,
+        options = {
+          parsers = {
+            css = true,
+            css_fn = true,
+          },
+          css_var_rgb = { enable = true }, -- CSS vars with R,G,B (e.g. --color: 240,198,198)
+          css_var = {
+            enable = true, -- resolve var(--name) references to their defined color
+            parsers = { css = true }, -- parsers for resolving variable values
+          },
+        },
+      }
+
+      require('colorizer').setup {
+        filetypes = { 'sass' },
+        buftypes = {},
+        user_commands = true,
+        lazy_load = true,
+        options = {
+          sass = {
+            enable = true, -- parse Sass color variables
+            parsers = { css = true }, -- parsers for resolving variable values
+            variable_pattern = '^%$([%w_-]+)', -- Lua pattern for variable names
+          },
+        },
+      }
+
+      -- We use tailwind ls instead
+      -- require('colorizer').setup {
+      --   filetypes = { 'html', "typescriptreact", 'javascriptreact' },
+      --   buftypes = {},
+      --   user_commands = true,
+      --   lazy_load = true,
+      --   options = {
+      --     parsers = {
+      --     },
+      --   },
+      -- }
+    end,
   },
 }
