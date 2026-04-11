020e84a1346d16abd2f4d633a434d62bf8d26777
cubewhy <qby140326@gmail.com>
2026-04-11 19:30:12 +0800

    chore: lazy load nvim-dap-virtual-text plugin

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -213,6 +213,7 @@ return {
   },
   {
     'theHamsta/nvim-dap-virtual-text',
+    event = 'VeryLazy',
     dependencies = {
       'mfussenegger/nvim-dap',
       'nvim-treesitter/nvim-treesitter',
