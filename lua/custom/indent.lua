4c53e6cbc694cd36cdb11dfe733599e3ef891f77
cubewhy <qby140326@gmail.com>
2026-04-11 12:00:07 +0800

    feat: use tab for Makefile

--- lua/custom/indent.lua
+++ lua/custom/indent.lua
@@ -27,3 +27,12 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.tabstop = 4
   end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'make',
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.shiftwidth = 8
+    vim.opt_local.tabstop = 8
+  end,
+})
