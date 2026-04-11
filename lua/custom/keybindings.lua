6f03b9323042736d46f852cc2d8e1cb70fbcfd04
cubewhy <qby140326@gmail.com>
2026-04-11 09:58:49 +0800

    refactor: extract vim.keymap.set to map

--- lua/custom/keybindings.lua
+++ lua/custom/keybindings.lua
@@ -1,11 +1,12 @@
+local map = vim.keymap.set
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR><Esc>', { noremap = true, silent = true })
+map('n', '<Esc>', '<cmd>nohlsearch<CR><Esc>', { noremap = true, silent = true })
 
-vim.keymap.set('n', '<leader>xq', vim.diagnostic.setloclist, { desc = 'Open diagnostic Quickfix list' })
+map('n', '<leader>xq', vim.diagnostic.setloclist, { desc = 'Open diagnostic Quickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -13,78 +14,65 @@ vim.keymap.set('n', '<leader>xq', vim.diagnostic.setloclist, { desc = 'Open diag
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+-- map('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- map('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- map('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- map('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- vim.keymap.set({ 'n', 'i', 'v' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save File' })
--- vim.keymap.set({ 'n', 'i', 'v' }, '<C-a>', '<cmd>wa<cr><esc>', { desc = 'Save All Files' })
+map('n', '<leader>-', '<C-w>s', { desc = 'Split Below' })
+map('n', '<leader>|', '<C-w>v', { desc = 'Split Right' })
+map('n', '<leader>w-', '<C-w>s', { desc = 'Split Window Below' })
+map('n', '<leader>w|', '<C-w>v', { desc = 'Split Window Right' })
+map('n', '<leader>wd', '<C-w>c', { desc = 'Close Window' })
+map('n', '<leader>ww', '<C-w>w', { desc = 'Switch Window' })
+map('n', '<leader>wh', '<C-w>h', { desc = 'Move to Left' })
+map('n', '<leader>wj', '<C-w>j', { desc = 'Move Down' })
+map('n', '<leader>wk', '<C-w>k', { desc = 'Move Up' })
+map('n', '<leader>wl', '<C-w>l', { desc = 'Move to Right' })
+map('n', '<leader>wo', '<C-w>o', { desc = 'Close Other Windows' })
+map('n', '<leader>w=', '<C-w>=', { desc = 'Equalize size' })
+map('n', '<leader>w>', '<cmd>vertical resize +5<cr>', { desc = 'Increase Width' })
+map('n', '<leader>w<', '<cmd>vertical resize -5<cr>', { desc = 'Decrease Width' })
 
-vim.keymap.set('n', '<leader>-', '<C-w>s', { desc = 'Split Below' })
-vim.keymap.set('n', '<leader>|', '<C-w>v', { desc = 'Split Right' })
-vim.keymap.set('n', '<leader>w-', '<C-w>s', { desc = 'Split Window Below' })
-vim.keymap.set('n', '<leader>w|', '<C-w>v', { desc = 'Split Window Right' })
-vim.keymap.set('n', '<leader>wd', '<C-w>c', { desc = 'Close Window' })
-vim.keymap.set('n', '<leader>ww', '<C-w>w', { desc = 'Switch Window' })
-vim.keymap.set('n', '<leader>wh', '<C-w>h', { desc = 'Move to Left' })
-vim.keymap.set('n', '<leader>wj', '<C-w>j', { desc = 'Move Down' })
-vim.keymap.set('n', '<leader>wk', '<C-w>k', { desc = 'Move Up' })
-vim.keymap.set('n', '<leader>wl', '<C-w>l', { desc = 'Move to Right' })
-vim.keymap.set('n', '<leader>wo', '<C-w>o', { desc = 'Close Other Windows' })
-vim.keymap.set('n', '<leader>w=', '<C-w>=', { desc = 'Equalize size' })
-vim.keymap.set('n', '<leader>w>', '<cmd>vertical resize +5<cr>', { desc = 'Increase Width' })
-vim.keymap.set('n', '<leader>w<', '<cmd>vertical resize -5<cr>', { desc = 'Decrease Width' })
+map('n', '[e', function()
+  vim.diagnostic.jump {
+    count = -1,
+    severity = vim.diagnostic.severity.ERROR,
+  }
+end, { desc = 'Go to previous Error' })
 
-vim.keymap.set(
-  'n',
-  '[e',
-  function()
-    vim.diagnostic.jump {
-      count = -1,
-      severity = vim.diagnostic.severity.ERROR,
-    }
-  end,
-  { desc = 'Go to previous Error' }
-)
+map('n', ']e', function()
+  vim.diagnostic.jump {
+    count = 1,
+    severity = vim.diagnostic.severity.ERROR,
+  }
+end, { desc = 'Go to next Error' })
 
-vim.keymap.set(
-  'n',
-  ']e',
-  function()
-    vim.diagnostic.jump {
-      count = 1,
-      severity = vim.diagnostic.severity.ERROR,
-    }
-  end,
-  { desc = 'Go to next Error' }
-)
-
-vim.keymap.set('n', '[w', function()
+map('n', '[w', function()
   vim.diagnostic.jump {
     count = -1,
   }
 end, { desc = 'Go to previous Warning/Diagnostic' })
 
-vim.keymap.set('n', ']w', function()
+map('n', ']w', function()
   vim.diagnostic.jump {
     count = 1,
   }
 end, { desc = 'Go to next Warning/Diagnostic' })
 
-vim.keymap.set(
+map(
   'n',
   '[h',
   function()
@@ -97,7 +85,7 @@ vim.keymap.set(
   { desc = 'Go to previous Hint' }
 )
 
-vim.keymap.set('n', ']h', function()
+map('n', ']h', function()
   vim.diagnostic.jump {
     count = 1,
     float = { border = 'none' },
@@ -105,11 +93,11 @@ vim.keymap.set('n', ']h', function()
 end, { desc = 'Go to next Hint' })
 
 -- indent
-vim.keymap.set('n', '<A-h>', '<<', { desc = 'Indent left' })
-vim.keymap.set('v', '<A-h>', '<gv', { desc = 'Indent left' })
+map('n', '<A-h>', '<<', { desc = 'Indent left' })
+map('v', '<A-h>', '<gv', { desc = 'Indent left' })
 
-vim.keymap.set('n', '<A-l>', '>>', { desc = 'Indent right' })
-vim.keymap.set('v', '<A-l>', '>gv', { desc = 'Indent right' })
+map('n', '<A-l>', '>>', { desc = 'Indent right' })
+map('v', '<A-l>', '>gv', { desc = 'Indent right' })
 
 vim.keymap.del('n', 'grn')
 vim.keymap.del({ 'n', 'x' }, 'gra')
