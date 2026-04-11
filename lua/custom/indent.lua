d20ac866c6082e46441b94e184a1fb3d4eca347c
cubewhy <qby140326@gmail.com>
2026-04-11 11:59:08 +0800

    feat: use tab for golang

--- lua/custom/indent.lua
+++ lua/custom/indent.lua
@@ -18,3 +18,12 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'python', 'rust', 'lua' },
   callback = function() set_indent(4) end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'go',
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+  end,
+})
