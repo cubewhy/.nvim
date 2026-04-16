6f3d7d8b6f76f5dedba0e73d6b8f793200425c2f
cubewhy <qby140326@gmail.com>
2026-04-16 18:41:17 +0800

    chore: disable mini.surround

--- init.lua
+++ init.lua
@@ -190,7 +190,7 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      -- require('mini.surround').setup()
       require('mini.move').setup {
         mappings = {
           left = '<M-h>',
