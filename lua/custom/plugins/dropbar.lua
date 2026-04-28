a05a923786d9a40ba8ed3ac0af7e9813f032e8cf
cubewhy <qby140326@gmail.com>
2026-04-28 18:20:44 +0800

    fix: use the dropbar fork

--- lua/custom/plugins/dropbar.lua
+++ lua/custom/plugins/dropbar.lua
@@ -1,16 +1,18 @@
 return {
   {
-    'Bekaboo/dropbar.nvim',
-    -- optional, but required for fuzzy finder support
+    'cubewhy/dropbar.nvim',
+    branch = 'fix-event',
     dependencies = {
       'nvim-telescope/telescope-fzf-native.nvim',
-      build = 'make'
+      build = 'make',
     },
-    config = function()
-      local dropbar_api = require('dropbar.api')
+    opts = {},
+    config = function(_, opts)
+      require('dropbar').setup(opts)
+      local dropbar_api = require 'dropbar.api'
       vim.keymap.set('n', '<leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
       vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
       vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
-    end
-  }
+    end,
+  },
 }
