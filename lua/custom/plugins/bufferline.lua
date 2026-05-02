128ea24bbb638f2dcfdc9b3ef39ba7531f65d22e
cubewhy <qby140326@gmail.com>
2026-05-02 11:09:03 +0800

    feat: O(1) bufferline diagnostics

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -6,21 +6,99 @@ return {
     config = function()
       vim.opt.showtabline = 2
 
+      local conditions = require 'heirline.conditions'
       local utils = require 'heirline.utils'
 
       local colors = {
-        bright_orange = utils.get_highlight('DiagnosticWarn').fg,
+        bright_bg = utils.get_highlight('Folded').bg,
+        bright_fg = utils.get_highlight('Folded').fg,
         red = utils.get_highlight('DiagnosticError').fg,
-        dark_red = utils.get_highlight('DiffDelete').fg,
+        dark_red = utils.get_highlight('DiffDelete').bg,
+        green = utils.get_highlight('String').fg,
+        blue = utils.get_highlight('Function').fg,
         gray = utils.get_highlight('NonText').fg,
-        bg = utils.get_highlight('TabLine').bg,
-        tabline_sel_bg = utils.get_highlight('TabLineSel').bg,
-        tabline_sel_fg = utils.get_highlight('TabLineSel').fg,
+        orange = utils.get_highlight('Constant').fg,
+        purple = utils.get_highlight('Statement').fg,
+        cyan = utils.get_highlight('Special').fg,
+        diag_warn = utils.get_highlight('DiagnosticWarn').fg,
+        diag_error = utils.get_highlight('DiagnosticError').fg,
+        diag_hint = utils.get_highlight('DiagnosticHint').fg,
+        diag_info = utils.get_highlight('DiagnosticInfo').fg,
+        git_del = utils.get_highlight('diffDeleted').fg,
+        git_add = utils.get_highlight('diffAdded').fg,
+        git_change = utils.get_highlight('diffChanged').fg,
       }
 
       local TablineBufnr = {
         provider = function(self) return tostring(self.bufnr) .. '. ' end,
         hl = 'Comment',
+      }
+
+      vim.diagnostic.config {
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = '',
+            [vim.diagnostic.severity.WARN] = ' ',
+            [vim.diagnostic.severity.INFO] = '󰋇 ',
+            [vim.diagnostic.severity.HINT] = '󰌵',
+          },
+        },
+      }
+
+      local Diagnostics = {
+        condition = function(self) return #vim.diagnostic.get(self.bufnr) > 0 end,
+
+        -- Fetching custom diagnostic icons
+        static = {
+          error_icon = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.ERROR],
+          warn_icon = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.WARN],
+          info_icon = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.INFO],
+          hint_icon = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.HINT],
+        },
+
+        -- If you defined custom LSP diagnostics with vim.fn.sign_define(), use this instead
+        -- Note defining custom LSP diagnostic this way its deprecated, though
+        --static = {
+        --    error_icon = vim.fn.sign_getdefined("DiagnosticSignError")[1].text,
+        --    warn_icon = vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text,
+        --    info_icon = vim.fn.sign_getdefined("DiagnosticSignInfo")[1].text,
+        --    hint_icon = vim.fn.sign_getdefined("DiagnosticSignHint")[1].text,
+        --},
+
+        init = function(self)
+          self.errors = #vim.diagnostic.get(self.bufnr, { severity = vim.diagnostic.severity.ERROR })
+          self.warnings = #vim.diagnostic.get(self.bufnr, { severity = vim.diagnostic.severity.WARN })
+          self.hints = #vim.diagnostic.get(self.bufnr, { severity = vim.diagnostic.severity.HINT })
+          self.info = #vim.diagnostic.get(self.bufnr, { severity = vim.diagnostic.severity.INFO })
+        end,
+
+        update = { 'DiagnosticChanged', 'BufEnter' },
+
+        {
+          provider = ' ',
+        },
+        {
+          provider = function(self)
+            -- 0 is just another output, we can decide to print it or not!
+            return self.errors > 0 and (self.error_icon .. self.errors .. ' ')
+          end,
+          hl = { fg = 'diag_error' },
+        },
+        {
+          provider = function(self) return self.warnings > 0 and (self.warn_icon .. self.warnings .. ' ') end,
+          hl = { fg = 'diag_warn' },
+        },
+        {
+          provider = function(self) return self.info > 0 and (self.info_icon .. self.info .. ' ') end,
+          hl = { fg = 'diag_info' },
+        },
+        {
+          provider = function(self) return self.hints > 0 and (self.hint_icon .. self.hints) end,
+          hl = { fg = 'diag_hint' },
+        },
+        {
+          provider = ' ',
+        },
       }
 
       local TablineFileName = {
@@ -51,6 +129,7 @@ return {
         { provider = '  ' },
         TablineFileIcon,
         TablineFileName,
+        Diagnostics,
         TablineModifiedIndicator,
         { provider = '  ' },
         hl = function(self)
@@ -85,6 +164,7 @@ return {
         { provider = ' ', hl = { bg = colors.bg } }
       )
 
+      require('heirline').load_colors(colors)
       require('heirline').setup {
         tabline = { TablineOffset, BufferLine },
       }
