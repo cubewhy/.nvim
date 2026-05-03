41f48d7f70a3c7add17a21bfa81e3aa5260dbb6c
cubewhy <qby140326@gmail.com>
2026-05-03 10:53:50 +0800

    chore: change keybinding desc

--- lua/custom/plugins/session.lua
+++ lua/custom/plugins/session.lua
@@ -5,7 +5,7 @@ return {
     keys = {
       { '<leader>qs', function() require('persistence').load() end, desc = 'Restore Session' },
       { '<leader>qS', function() require('persistence').select() end, desc = 'Select Session' },
-      { '<leader>ql', function() require('persistence').load { last = true } end, desc = 'Restore [L]ast Session' },
+      { '<leader>ql', function() require('persistence').load { last = true } end, desc = 'Restore Last Session' },
       { '<leader>qd', function() require('persistence').stop() end, desc = "Don't Save Current Session" },
       { '<leader>qq', '<cmd>qa<cr>', desc = 'Quit all' },
     },
