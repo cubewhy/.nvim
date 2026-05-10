c1a81f84643ee89808f4115b31f9c6768fed87d6
cubewhy <qby140326@gmail.com>
2026-05-10 12:25:00 +0800

    feat: smart find files

--- lua/custom/plugins/picker.lua
+++ lua/custom/plugins/picker.lua
@@ -40,7 +40,7 @@ return {
       { '<leader>/', function() Snacks.picker.grep() end, desc = 'Grep In Workspace' },
       { '<leader>su', function() Snacks.picker.undo() end, desc = 'Search Undo history' },
       { '<leader>s.', function() Snacks.picker.recent() end, desc = 'Search Recent Files' },
-      { '<leader><leader>', function() Snacks.picker.buffers() end, desc = 'Find existing buffers' },
+      { '<leader><leader>', function() Snacks.picker.smart() end, desc = 'Smart Find Files' },
       { '<leader>sw', function() Snacks.picker.grep_word() end, desc = 'Search current Word', mode = { 'n', 'v' } },
 
       -- Specialized Search
@@ -48,28 +48,18 @@ return {
       { '<leader>sb', function() Snacks.picker.lines() end, desc = 'Buffer Fuzzy Find' },
       { '<leader>s/', function() Snacks.picker.grep_buffers() end, desc = 'Search in Open Files' },
       { '<leader>uC', function() Snacks.picker.colorschemes() end, desc = 'Colorschemes' },
+
+      -- LSP
+      { 'gd', function() Snacks.picker.lsp_definitions() end, desc = 'Goto Definition' },
+      { 'gD', function() Snacks.picker.lsp_declarations() end, desc = 'Goto Declaration' },
+      { 'gr', function() Snacks.picker.lsp_references() end, nowait = true, desc = 'References' },
+      { 'gI', function() Snacks.picker.lsp_implementations() end, desc = 'Goto Implementation' },
+      { 'gy', function() Snacks.picker.lsp_type_definitions() end, desc = 'Goto T[y]pe Definition' },
+      { 'gai', function() Snacks.picker.lsp_incoming_calls() end, desc = 'C[a]lls Incoming' },
+      { 'gao', function() Snacks.picker.lsp_outgoing_calls() end, desc = 'C[a]lls Outgoing' },
+      { '<leader>ss', function() Snacks.picker.lsp_symbols() end, desc = 'LSP Symbols' },
+      { '<leader>sS', function() Snacks.picker.lsp_workspace_symbols() end, desc = 'LSP Workspace Symbols' },
     },
-    config = function(_, opts)
-      require('snacks').setup(opts)
-
-      -- [[ LSP Keybindings via Autocmd ]]
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('snacks-lsp-attach', { clear = true }),
-        callback = function(event)
-          local buf = event.buf
-
-          -- Navigation
-          vim.keymap.set('n', 'gd', function() Snacks.picker.lsp_definitions() end, { buffer = buf, desc = 'Goto Definition' })
-          vim.keymap.set('n', 'gr', function() Snacks.picker.lsp_references() end, { buffer = buf, desc = 'Goto References' })
-          vim.keymap.set('n', 'gI', function() Snacks.picker.lsp_implementations() end, { buffer = buf, desc = 'Goto Implementation' })
-          vim.keymap.set('n', 'gt', function() Snacks.picker.lsp_type_definitions() end, { buffer = buf, desc = 'Type Definition' })
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = buf, desc = 'Goto Declaration' })
-
-          -- Symbol Search
-          vim.keymap.set('n', '<leader>ss', function() Snacks.picker.lsp_symbols() end, { buffer = buf, desc = 'Search Document Symbols' })
-          vim.keymap.set('n', '<leader>sS', function() Snacks.picker.lsp_workspace_symbols() end, { buffer = buf, desc = 'Search Workspace Symbols' })
-        end,
-      })
-    end,
+    config = function(_, opts) require('snacks').setup(opts) end,
   },
 }
