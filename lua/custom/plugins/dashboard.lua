ef79a92f17c6be65fc0e34792fa5087008f7ec18
cubewhy <qby140326@gmail.com>
2026-05-10 11:54:50 +0800

    refactor: replace alpha-nvim with snacks

--- lua/custom/plugins/dashboard.lua
+++ lua/custom/plugins/dashboard.lua
@@ -1,22 +1,91 @@
 return {
   {
-    'goolord/alpha-nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      local alpha = require 'alpha'
-      local startify = require 'alpha.themes.startify'
-
-      startify.section.top_buttons.val = {
-        startify.button('n', '󰈔 New File', '<cmd>enew<CR>'),
-        startify.button('s', ' Restore Session', '<cmd>lua require("persistence").load()<CR>'),
-        startify.button('l', '󰒲 Lazy', '<cmd>Lazy<CR>'),
-        startify.button('u', '󰊳 Update', '<cmd>Lazy update<CR>'),
-        startify.button('q', ' Quit', '<cmd>qa<CR>'),
-      }
-
-      startify.file_icons.provider = 'devicons'
-
-      alpha.setup(startify.config)
-    end,
+    'folke/snacks.nvim',
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      ---@class snacks.dashboard.Config
+      dashboard =
+        ---@field enabled? boolean
+        ---@field sections snacks.dashboard.Section
+        ---@field formats table<string, snacks.dashboard.Text|fun(item:snacks.dashboard.Item, ctx:snacks.dashboard.Format.ctx):snacks.dashboard.Text>
+        {
+          width = 60,
+          row = nil, -- dashboard position. nil for center
+          col = nil, -- dashboard position. nil for center
+          pane_gap = 4, -- empty columns between vertical panes
+          autokeys = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', -- autokey sequence
+          -- These settings are used by some built-in sections
+          preset = {
+            -- Defaults to a picker that supports `fzf-lua`, `telescope.nvim` and `mini.pick`
+            ---@type fun(cmd:string, opts:table)|nil
+            pick = nil,
+            -- Used by the `keys` section to show keymaps.
+            -- Set your custom keymaps here.
+            -- When using a function, the `items` argument are the default keymaps.
+            ---@type snacks.dashboard.Item[]
+            keys = {
+              { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
+              { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
+              { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
+              { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
+              { icon = ' ', key = 'c', desc = 'Config', action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+              { icon = ' ', key = 's', desc = 'Restore Session', section = 'session' },
+              { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
+              { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+            },
+            -- Used by the `header` section
+            header = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+          },
+          -- item field formatters
+          formats = {
+            icon = function(item)
+              if item.file and item.icon == 'file' or item.icon == 'directory' then return Snacks.dashboard.icon(item.file, item.icon) end
+              return { item.icon, width = 2, hl = 'icon' }
+            end,
+            footer = { '%s', align = 'center' },
+            header = { '%s', align = 'center' },
+            file = function(item, ctx)
+              local fname = vim.fn.fnamemodify(item.file, ':~')
+              fname = ctx.width and #fname > ctx.width and vim.fn.pathshorten(fname) or fname
+              if #fname > ctx.width then
+                local dir = vim.fn.fnamemodify(fname, ':h')
+                local file = vim.fn.fnamemodify(fname, ':t')
+                if dir and file then
+                  file = file:sub(-(ctx.width - #dir - 2))
+                  fname = dir .. '/…' .. file
+                end
+              end
+              local dir, file = fname:match '^(.*)/(.+)$'
+              return dir and { { dir .. '/', hl = 'dir' }, { file, hl = 'file' } } or { { fname, hl = 'file' } }
+            end,
+          },
+          sections = {
+            { section = 'header' },
+            { section = 'keys', gap = 1, padding = 1 },
+            { pane = 2, icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+            { pane = 2, icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+            {
+              pane = 2,
+              icon = ' ',
+              title = 'Git Status',
+              section = 'terminal',
+              enabled = function() return Snacks.git.get_root() ~= nil end,
+              cmd = 'git status --short --branch --renames',
+              height = 5,
+              padding = 1,
+              ttl = 5 * 60,
+              indent = 3,
+            },
+            { section = 'startup' },
+          },
+        },
+    },
   },
 }
