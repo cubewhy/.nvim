0ca504ceeea91f8315b19f7035b92e635d56f0cd
cubewhy <qby140326@gmail.com>
2026-05-13 20:56:58 +0800

    feat(todo): add keybinding to navigate to next/prev todo comment

--- lua/custom/plugins/todo.lua
+++ lua/custom/plugins/todo.lua
@@ -1,8 +1,15 @@
 return {
   'folke/todo-comments.nvim',
+  event = 'BufEnter',
   keys = {
     { '<leader>st', function() Snacks.picker.todo_comments() end, desc = 'Todo' },
     { '<leader>sT', function() Snacks.picker.todo_comments { keywords = { 'TODO', 'FIX', 'FIXME' } } end, desc = 'Todo/Fix/Fixme' },
   },
   opts = {},
+  config = function(_, opts)
+    require('todo-comments').setup(opts)
+
+    vim.keymap.set('n', ']t', function() require('todo-comments').jump_next() end, { desc = 'Next todo comment' })
+    vim.keymap.set('n', '[t', function() require('todo-comments').jump_prev() end, { desc = 'Previous todo comment' })
+  end,
 }
