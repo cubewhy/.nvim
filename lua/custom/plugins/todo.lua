8297494dd2382a9441ecdba0fb0e99862ec37586
cubewhy <qby140326@gmail.com>
2026-05-13 18:48:44 +0800

    fix: add opts to call setup for todo-comments

--- lua/custom/plugins/todo.lua
+++ lua/custom/plugins/todo.lua
@@ -4,4 +4,5 @@ return {
     { '<leader>st', function() Snacks.picker.todo_comments() end, desc = 'Todo' },
     { '<leader>sT', function() Snacks.picker.todo_comments { keywords = { 'TODO', 'FIX', 'FIXME' } } end, desc = 'Todo/Fix/Fixme' },
   },
+  opts = {},
 }
