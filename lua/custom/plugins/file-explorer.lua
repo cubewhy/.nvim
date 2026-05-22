f986fb865657efc7071c78ce68b280553509a617
cubewhy <qby140326@gmail.com>
2026-05-22 16:39:49 +0800

    feat(oil): add lsp file methods options

--- lua/custom/plugins/file-explorer.lua
+++ lua/custom/plugins/file-explorer.lua
@@ -19,7 +19,7 @@ return {
       },
       view_options = {
         show_hidden = false,
-        is_hidden_file = function(name, bufnr)
+        is_hidden_file = function(name, _)
           local ignored = { 'node_modules', '.direnv', '.git' }
           for _, pattern in ipairs(ignored) do
             if name == pattern then return true end
@@ -43,6 +43,15 @@ return {
       constrain_cursor = 'editable',
       -- Set to true to watch the filesystem for changes and reload oil
       watch_for_changes = true,
+      lsp_file_methods = {
+        -- Enable or disable LSP file operations
+        enabled = true,
+        -- Time to wait for LSP file operations to complete before skipping
+        timeout_ms = 1000,
+        -- Set to true to autosave buffers that are updated with LSP willRenameFiles
+        -- Set to "unmodified" to only save unmodified buffers
+        autosave_changes = 'unmodified',
+      },
       keymaps = {
         ['g?'] = 'show_help',
         ['<CR>'] = 'actions.select',
