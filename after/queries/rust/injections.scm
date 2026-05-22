8f3404f2dfcdd6d522598ba78fecc45ce6174e4e
cubewhy <qby140326@gmail.com>
2026-05-22 12:32:32 +0800

    feat: add sqlx injection

---
+++ after/queries/rust/injections.scm
@@ -0,0 +1,16 @@
+; extends
+
+(macro_invocation
+  macro: [
+    (scoped_identifier name: (_) @_macro_name)
+    (identifier) @_macro_name
+  ]
+  (token_tree
+    [
+      (raw_string_literal (string_content) @injection.content)
+      (string_literal) @injection.content
+    ]
+  )
+  (#match? @_macro_name "^query(_as|_scalar)?$")
+  (#set! injection.language "sql")
+)
