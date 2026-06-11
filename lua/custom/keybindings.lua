5002f8c245db7eda721b0bdae672b595a894b4c7
cubewhy <qby140326@gmail.com>
2026-06-11 20:12:22 +0800

    chore: add keybinding to close split

--- lua/custom/keybindings.lua
+++ lua/custom/keybindings.lua
@@ -36,6 +36,8 @@ map('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
 map('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
 map('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
 
+map('n', '<leader>wd', '<cmd>close<CR>', { desc = 'Close split' })
+
 map('n', '<leader>-', '<C-w>s', { desc = 'Split Below' })
 map('n', '<leader>|', '<C-w>v', { desc = 'Split Right' })
 
