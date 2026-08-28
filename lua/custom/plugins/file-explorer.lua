2d758aec53c9ce2eaa6eb24da451790d8efc1dc2
cubewhy <qby140326@gmail.com>
2026-08-28 09:07:17 +0800

    feat: add snakes explorer

--- lua/custom/plugins/file-explorer.lua
+++ lua/custom/plugins/file-explorer.lua
@@ -81,4 +81,26 @@ return {
       })
     end,
   },
+  {
+    'folke/snacks.nvim',
+    keys = {
+      { '<leader>e', mode = '', function() Snacks.explorer() end, desc = 'Explore files' },
+    },
+    ---@type snacks.Config
+    opts = {
+      explorer = {
+        -- your explorer configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+      picker = {
+        sources = {
+          explorer = {
+            -- your explorer picker configuration comes here
+            -- or leave it empty to use the default settings
+          },
+        },
+      },
+    },
+  },
 }
